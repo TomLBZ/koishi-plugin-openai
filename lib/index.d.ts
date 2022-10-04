4d6c46866665dd05525332a8c41d5531888ff148
TomLBZ <libozhao142857@hotmail.com>
2022-10-04 21:35:03 +0800

    built package

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 import { Context, Schema } from 'koishi';
-export declare const name = "koishi-plugin-openai";
+export declare const name = "openai";
 export interface Config {
     apikey: string;
     botname: string;
